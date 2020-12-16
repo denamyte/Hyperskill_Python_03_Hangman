edd9ac18099a7d0262011431317502dde2789ca4
denamyte <denamyte@gmail.com>
2020-12-17 01:38:52 +0200

    2020-12-17: Stage 8/8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,6 +1,13 @@
 import random
 
 
+def play_or_exit() -> str:
+    result = ''
+    while result not in menu_options:
+        result = input('Type "play" to play the game, "exit" to quit: ')
+    return result
+
+
 def get_hint_str():
     mapped_word = map(lambda char: char if char in guessed_letters else '-', word)
     return ''.join(mapped_word)
@@ -20,28 +27,33 @@ def english_lowercase(letter) -> bool:
     return False
 
 
-word = random.choice(['python', 'java', 'kotlin', 'javascript'])
-guessed_letters = set()
-lives = 8
-hint = get_hint_str()
 print('H A N G M A N')
-while lives > 0:
-    print('\n' + hint)
-    user_letter = input('Input a letter: ')
+menu_options = ['play', 'exit']
 
-    if single(user_letter) and english_lowercase(user_letter):
-        if user_letter in guessed_letters:
-            print("You've already guessed this letter")
-        else:
-            if user_letter not in word:
-                print("That letter doesn't appear in the word")
-                lives -= 1
-            guessed_letters.add(user_letter)
-
+while play_or_exit() == menu_options[0]:
+    word = random.choice(['python', 'java', 'kotlin', 'javascript'])
+    guessed_letters = set()
+    lives = 8
     hint = get_hint_str()
-    if '-' not in hint:
-        print(f'''You guessed the word {word}!
-You survived!''')
-        break
-else:
-    print("You lost!")
+
+    while lives > 0:
+        print('\n' + hint)
+        user_letter = input('Input a letter: ')
+
+        if single(user_letter) and english_lowercase(user_letter):
+            if user_letter in guessed_letters:
+                print("You've already guessed this letter")
+            else:
+                if user_letter not in word:
+                    print("That letter doesn't appear in the word")
+                    lives -= 1
+                guessed_letters.add(user_letter)
+
+        hint = get_hint_str()
+        if '-' not in hint:
+            print(f'''You guessed the word {word}!
+You survived!
+''')
+            break
+    else:
+        print('You lost!\n')
